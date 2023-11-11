792bfbdeae1f1119e0869d6be3147d6fe872d8ea
Alex M <alex@merchant.dev>
2023-11-11 14:56:07 -0600

    TASK #2 - Model Revisions - Employer
    * Added @Entity annotation above class defintion
    * Added new 'location' field to class
    * Added public accessor methods for 'location' field
    * Added validation annotations to 'location' field to ensure value
        cannot be null, blank, or more than 75 characters in length.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @NotBlank
+    @Size(message = "The 'location' field value cannot exceed 75 characters", max = 75)
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
