2df278ac43e2a47d18ac7d7c41fd91db8964a816
Alex M <alex@merchant.dev>
2023-11-11 13:55:32 -0600

    TASK #1 - SQL Comments Added
    * Specified the column names and their data types per Task #1
        instructions

--- queries.sql
+++ queries.sql
@@ -1,4 +1,18 @@
 --Part 1
+/*
+ * Column: 'id'
+ * Type: INTEGER
+ * PRIMARY KEY
+ 
+ * Column: 'employer'
+ * Type: VARCHAR(255)
+  
+ * Column: 'name'
+ * Type: VARCHAR(255)
+  
+ * Column: 'skills'
+ * Type: VARCHAR(255)
+*/
 
 --Part 2
 
