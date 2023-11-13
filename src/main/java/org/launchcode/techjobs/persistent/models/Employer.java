be809d4ac210b0f4e6081fbd75052eb570992fe0
Alex M <alex@merchant.dev>
2023-11-12 18:09:04 -0600

    TASK #3 - Employer.jobs
    * Added new 'jobs' field
    * Set up persistance annotations oon 'jobs' field to indicate ownership
        and the 'One' side of the OneToMany relationship

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -12,6 +17,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(message = "The 'location' field value cannot exceed 75 characters", max = 75)
     private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
